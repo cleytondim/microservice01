23efb976732642ea2c5267954f9e28fb565f2efd
Cleyton Dim <cleytondim@gmail.com>
2021-10-31 03:24:47 -0300

    Feign Client

--- hr-payroll/src/main/java/com/dimsoluts/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/dimsoluts/hrpayroll/services/PaymentService.java
@@ -1,31 +1,36 @@
 package com.dimsoluts.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.dimsoluts.hrpayroll.entities.Payment;
 import com.dimsoluts.hrpayroll.entities.Worker;
+import com.dimsoluts.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
+	/*
 	@Value("${hr-worker.host}")
 	private String workerHost;
 	
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
+	Map<String, String> uriVariables = new HashMap<>();
 		uriVariables.put("id", ""+workerId);
 		
 		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
 		
+	*/
+	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
+	public Payment getPayment(long workerId, int days) {
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
