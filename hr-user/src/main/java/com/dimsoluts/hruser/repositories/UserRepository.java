c82a7fd6fdc722b7744ae454cf4b9e313f71eb63
Cleyton Dim <cleytondim@gmail.com>
2021-11-02 20:37:17 -0300

    User Repository, User Resource, Zull Config

---
+++ hr-user/src/main/java/com/dimsoluts/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.dimsoluts.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dimsoluts.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+}
