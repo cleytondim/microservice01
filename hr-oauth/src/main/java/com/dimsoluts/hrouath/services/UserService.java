f96c8d41493d4570221bb31313495159c60f9a63
Cleyton Dim <cleytondim@gmail.com>
2021-11-02 22:21:11 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/dimsoluts/hrouath/services/UserService.java
@@ -0,0 +1,27 @@
+package com.dimsoluts.hrouath.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dimsoluts.hrouath.entities.User;
+import com.dimsoluts.hrouath.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail(email).getBody();
+		if (user == null){
+			logger.error("Email not found "+ email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email found: "+ email);
+		return user;
+	}
+}
